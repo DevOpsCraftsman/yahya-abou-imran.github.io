23248f27e15df13ca12f47b887635e4da0b1fd9c
Yahya Abou Imran <yahya-abou-imran@protonmail.com>
2022-09-10 09:25:17 +0100

    Improve languages display script

--- languages/script.py
+++ languages/script.py
@@ -5,32 +5,31 @@ def sort(languages):
      return sorted(languages, key=lambda lang: lang["date"])
 
 
+def display(languages):
+    for l in sort(languages):
+        s = f"- `[{('*' * l['level']).ljust(5)}]` {l['name']} ({l['date']})"
+        print(s)
+
+def extract(languages, field):
+    extracted = []
+    for lang in languages.copy():
+        if lang.get(field):
+            extracted.append(lang)
+            languages.remove(lang)
+    return extracted
+
+
 with open("./data.yaml") as file:
     data = yaml.safe_load(file)
 
-mainstreams: list = []
-
-lang: dict
-for lang in data.copy():
-    if lang.get("mainstream"):
-        mainstreams.append(lang)
-        data.remove(lang)
 
 print("## Mainstreams:")
-for lang in sort(mainstreams):
-    print(f"- `[{('*' * lang['level']).ljust(5)}]` {lang['name']} ({lang['date']})")
+display(extract(data, "mainstream"))
 
-
-for_prod = []
-for lang in data.copy():
-    if lang.get("prod"):
-        for_prod.append(lang)
-        data.remove(lang)
 
 print("\n## Interinting for prod:")
-for lang in sort(for_prod):
-    print(f"- `[{('*' * lang['level']).ljust(5)}]` {lang['name']} ({lang['date']})")
+display(extract(data, "prod"))
+
 
 print("\n## Others:")
-for lang in sort(data):
-    print(f"- `[{('*' * lang['level']).ljust(5)}]` {lang['name']} ({lang['date']})")
+display(data)
